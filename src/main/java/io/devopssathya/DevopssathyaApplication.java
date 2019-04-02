dc40738e66aeeb0b910651eba2fad449e1097cbf
Sathyanarayana Dudigama <sathyad4ubcexpert@gmail.com>
2019-04-03 00:51:44 +0530

    Initial commit to repo

---
+++ src/main/java/io/devopssathya/DevopssathyaApplication.java
@@ -0,0 +1,13 @@
+package io.devopssathya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopssathyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopssathyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/devopssathya/DevopssathyaApplicationTests.java
@@ -0,0 +1,16 @@
+package io.devopssathya;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopssathyaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
